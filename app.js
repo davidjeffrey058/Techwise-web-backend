7814f2eeace27d17147c3bbd1ec5696af2b0cc32
davidjeffrey058 <113363460+davidjeffrey058@users.noreply.github.com>
2024-03-20 15:05:21 +0100

    Added some get requests

--- app.js
+++ app.js
@@ -1,11 +1,84 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
+const app = express();
+const uri = "mongodb://localhost:27017/techwise";
 
-const App = express();
+app.use(express.json());
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:3000', 'http://10.206.35.1:3000'];
+  const origin = req.headers.origin;
 
-App.get("/", (req, res) => {
-  res.end("This is the message");
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
-App.listen(3000, () => {
-  console.log("listening at port 3000");
-});
+MongoClient.connect(uri)
+  .then((client) => {
+    app.get("/", (req, res) => {
+      res.redirect("/techwise-api");
+    });
+
+    // Get all the products from the database
+    app.get("/techwise-api", (req, res) => {
+
+      client.db().collection("products")
+        .find()
+        .toArray()
+        .then((products) => {
+          res.status(200).json(products);
+        })
+        .catch(() => {
+          res.status(500).json({ error: "Unable to get data" });
+        });
+    });
+
+    // Get a single product by id
+    app.get('/techwise-api/:id', (req, res) => {
+      client.db().collection('products')
+        .findOne({ _id: req.params.id })
+        .then(data => {
+          res.status(200).json(data);
+        })
+        .catch(() => {
+          res.status(500).json({ error: "Unable to get the data" });
+        })
+    })
+
+    // Add a product to the database
+    app.post("/techwise-api", (req, res) => [
+      client
+        .db().collection("products")
+        .insertOne(req.body)
+        .then((response) => {
+          res.status(200).json(response);
+        })
+        .catch(() => {
+          res.status(500).json({ error: "Unable to add data" });
+        }),
+    ]);
+
+    // Get products by category
+    app.get('/techwise-api/category/:category', (req, res) => {
+      const category = req.params.category;
+      client.db().collection('products')
+        .find({ category: category })
+        .toArray()
+        .then(data => {
+          res.status(200).json(data);
+        })
+        .catch(() => {
+          res.status(500).json({ error: 'Can\'t get the data' });
+        })
+    })
+
+    app.listen(8080);
+  })
+  .catch((err) => {
+    throw err;
+  });
