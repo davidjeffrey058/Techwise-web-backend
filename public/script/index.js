ff39d42b6d52169110f000f6fd59abeed14a79cb
davidjeffrey058 <113363460+davidjeffrey058@users.noreply.github.com>
2024-08-28 19:08:27 +0000

    Added the primary function for the add category section

--- public/script/index.js
+++ public/script/index.js
@@ -1,6 +1,14 @@
-function toggleDrawer(){
+function toggleDrawer() {
     const drawer = document.querySelector('.mobile_drawer');
     const overlay = document.querySelector('.mobile_drawer_background');
     drawer.classList.toggle('active');
     overlay.classList.toggle('active');
+}
+
+function togglePopup() {
+    const popup = document.querySelector('.pop_up');
+    const popupBackground = document.querySelector('.pop_up_background');
+
+    popup.classList.toggle('active')
+    popupBackground.classList.toggle('active')
 }
